e89c95e31998b194138253844057ad1541dfc41d
nareshyadav45 <131270461+nareshyadav45@users.noreply.github.com>
2024-02-07 13:43:38 +0530

    Update OurController.java

--- src/main/java/com/exception/custom/controller/OurController.java
+++ src/main/java/com/exception/custom/controller/OurController.java
@@ -3,7 +3,7 @@ package com.exception.custom.controller;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RestController;
 
-@Component
+//@Component  
 public class OurController {
 public static String variable="String Energy";
 
