4cca1373339a3fd0a0a425e26cda117153e2a279
NareshYadavDev <chinthalanareshyadav@gmail.com>
2024-02-07 13:36:19 +0530

    annotation added

--- src/main/java/com/exception/custom/controller/OurController.java
+++ src/main/java/com/exception/custom/controller/OurController.java
@@ -1,8 +1,9 @@
 package com.exception.custom.controller;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Component
 public class OurController {
 public static String variable="String Energy";
 
